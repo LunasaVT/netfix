fb3627cc385d3a44d90f16df16a80f2d82bd3473
LunasaVT <me@lunasa.dev>
2026-04-12 14:48:47 +0530

    Fix MixinPacketByteBuf & bump to 1.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "dev.lunasa"
-version = "1.0.2"
+version = "1.0.3"
 
 val shadow by configurations.creating { isCanBeResolved = true }
 
--- src/main/java/dev/lunasa/netfix/mixin/minecraft/MixinPacketByteBuff.java
+++ src/main/java/dev/lunasa/netfix/mixin/minecraft/MixinPacketByteBuff.java
@@ -13,9 +13,6 @@ public abstract class MixinPacketByteBuff extends ByteBuf {
     @Shadow
     public abstract int readVarInt();
 
-    @Shadow
-    public abstract ByteBuf readBytes(int i);
-
     /**
      * @reason Remove unsafe {@link ByteBuf#array()}
      * @author Lunasa
@@ -29,15 +26,8 @@ public abstract class MixinPacketByteBuff extends ByteBuf {
         } else if (length < 0) {
             throw new DecoderException("The received encoded string buffer length is less than zero! Weird string!");
         } else {
-            byte[] bytes;
-
-            var buf = this.readBytes(length);
-
-            if (buf.hasArray()) bytes = buf.array(); else {
-                bytes = new byte[length];
-                buf.readBytes(length); buf.release(); // since this allocates a direct buffer
-            }
-
+            byte[] bytes = new byte[length];
+            this.readBytes(bytes);
             String string = new String(bytes, Charsets.UTF_8);
 
             if (string.length() > maxLength) {
